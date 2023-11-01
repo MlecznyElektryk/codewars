660ab2f358ff4bb8813d07585c97fdda536fe633
MlecznyElektryk <mlecznyelektryk@gmail.com>
2023-11-01 18:01:57 +0100

    Add a program that verifies if a word is a palindrome

---
+++ 8kyu/palindrome.cs
@@ -0,0 +1,17 @@
+﻿namespace palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "radar";
+            Console.WriteLine($"Is {word} palindrome? {isPalindrome(word)}");
+        }
+        static bool isPalindrome(string word)
+        {
+            char[] temp = word.ToCharArray();
+            Array.Reverse(temp);
+            return (new string(temp) == word) ? true : false;
+        }
+    }
+}
